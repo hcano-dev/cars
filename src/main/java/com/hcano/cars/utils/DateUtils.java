44fdcdc8e3b44326adb51ece013b679f4b879f4e
Héctor Cano <hcano@teralco.com>
2023-02-09 09:33:10 +0100

    add date utils

---
+++ src/main/java/com/hcano/cars/utils/DateUtils.java
@@ -0,0 +1,10 @@
+package com.hcano.cars.utils;
+
+import java.util.Date;
+
+public class DateUtils {
+    private DateUtils() { }
+    public static Date now() {
+        return new Date();
+    }
+}
