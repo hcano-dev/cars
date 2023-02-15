3ae0469e200b1d5f566c8906bdfca065dbaa5cb1
Héctor Cano <hcano@teralco.com>
2023-02-15 08:22:47 +0100

    move to test/controller path

--- src/test/java/com/hcano/cars/controller/HealthCheckControllerTest.java
+++ src/test/java/com/hcano/cars/controller/HealthCheckControllerTest.java
@@ -1,10 +1,9 @@
-package com.hcano.cars.controller.health_check;
+package com.hcano.cars.controller;
 
-import com.hcano.cars.controller.RequestTestCase;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
 
-public class HealthCheckGetControllerShould extends RequestTestCase {
+public class HealthCheckControllerTest extends RequestTestCase {
     @Test
     public void return_ok_status() throws Exception {
         assertResponse("/health-check", HttpStatus.OK.value(), "{'status':'ok'}");
