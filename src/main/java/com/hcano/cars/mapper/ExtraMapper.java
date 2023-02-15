79bd8d262aae77a438bef84c0fc145244daf41d6
Héctor Cano <hcano@teralco.com>
2023-02-15 17:41:16 +0100

    add mapper to covert to list of DTOs in ExtraMapper

--- src/main/java/com/hcano/cars/mapper/ExtraMapper.java
+++ src/main/java/com/hcano/cars/mapper/ExtraMapper.java
@@ -6,9 +6,16 @@ import com.hcano.cars.model.domain.Extra;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ExtraMapper {
     ExtraPayload toDTO(Extra extra);
 
     Extra toEntity(ExtraEditPayload extraEditPayload);
+
+    default List<ExtraPayload> toListOfDTOs(List<Extra> extras) {
+        return extras.stream().map(this::toDTO).collect(Collectors.toList());
+    }
 }
