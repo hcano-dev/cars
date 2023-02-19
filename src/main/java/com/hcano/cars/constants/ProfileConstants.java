da56e0333f68aa03260eeea4e37b74b0f6714e63
Héctor Cano <hcano@teralco.com>
2023-02-19 13:10:24 +0100

    add h2 database in test profile

---
+++ src/main/java/com/hcano/cars/constants/ProfileConstants.java
@@ -0,0 +1,11 @@
+package com.hcano.cars.constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ProfileConstants {
+
+    public static final String SPRING_PROFILE_TEST = "test";
+
+}
--- src/test/java/com/hcano/cars/controller/RequestTestCase.java
+++ src/test/java/com/hcano/cars/controller/RequestTestCase.java
@@ -1,10 +1,12 @@
 package com.hcano.cars.controller;
 
+import com.hcano.cars.constants.ProfileConstants;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
@@ -19,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles(profiles = ProfileConstants.SPRING_PROFILE_TEST)
 @Transactional
 public abstract class RequestTestCase {
     @Autowired
