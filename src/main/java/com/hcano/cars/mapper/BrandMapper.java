f0db08360897acf7b9d51cd92ceee7791a6d1f59
Héctor Cano <hcano@teralco.com>
2023-02-19 13:14:05 +0100

    add brand mapper to list of dtos

--- src/main/java/com/hcano/cars/mapper/BrandMapper.java
+++ src/main/java/com/hcano/cars/mapper/BrandMapper.java
@@ -6,9 +6,16 @@ import com.hcano.cars.model.domain.Brand;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface BrandMapper {
     BrandPayload toDTO(Brand brand);
 
     Brand toEntity(BrandEditPayload brandEditPayload);
+
+    default List<BrandPayload> toListOfDTOs(List<Brand> brands) {
+        return brands.stream().map(this::toDTO).collect(Collectors.toList());
+    }
 }
