26c0dd5352be75a3d4dfddfcc7ba6d4f2d9ac683
hcano-dev <hcano.dev@gmail.com>
2023-02-19 22:39:17 +0100

    add brand service unit tests

--- src/test/java/com/hcano/cars/service/BrandServiceTest.java
+++ src/test/java/com/hcano/cars/service/BrandServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 public class BrandServiceTest extends BaseServiceTest<Brand> {
@@ -25,6 +26,24 @@ public class BrandServiceTest extends BaseServiceTest<Brand> {
     }
 
     @Test
+    public void return_a_list_of_existing_brands() {
+        this.service.findAll();
+
+        verify(this.service.baseRepository, atLeastOnce()).findAll();
+    }
+
+    @Test
+    public void return_an_existing_brand() {
+        String id = IdentifierMother.random();
+
+        given(service.baseRepository.existsById(id)).willReturn(true);
+
+        this.service.findById(id);
+
+        verify(this.service.baseRepository, atLeastOnce()).findById(id);
+    }
+
+    @Test
     public void save_a_valid_brand() {
         String id = IdentifierMother.random();
         BrandEditPayload request = BrandRequestMother.random();
@@ -33,10 +52,17 @@ public class BrandServiceTest extends BaseServiceTest<Brand> {
 
         this.service.create(id, brand);
 
-        shouldHaveSaved(brand);
+        verify(this.service.baseRepository, atLeastOnce()).save(brand);
     }
 
-    private void shouldHaveSaved(Brand brand) {
-        verify(service.baseRepository, atLeastOnce()).save(brand);
+    @Test
+    public void delete_an_existing_brand() {
+        String id = IdentifierMother.random();
+
+        given(this.service.baseRepository.existsById(id)).willReturn(true);
+
+        this.service.delete(id);
+
+        verify(this.service.baseRepository, atLeastOnce()).deleteById(id);
     }
 }
