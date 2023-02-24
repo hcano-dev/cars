2a73aa0d01095ec8be8b0890ec6e606de0f06dc5
hcano-dev <114352716+hcano-dev@users.noreply.github.com>
2023-02-24 14:06:36 +0100

    Update JsonMapperUtils.java
    remove unnecessary code

--- src/main/java/com/hcano/cars/utils/JsonMapperUtils.java
+++ src/main/java/com/hcano/cars/utils/JsonMapperUtils.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonMapperUtils {
-    private JsonMapperUtils() { }
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
